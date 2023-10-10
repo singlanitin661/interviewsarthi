d263099aa1775d78b2aab98cfff0a4e22ec5e8f4
Nitin Singla <78803509+singlanitin661@users.noreply.github.com>
2023-10-10 15:48:08 +0530

    Create Dataset.py

---
+++ Factors/Dataset.py
@@ -0,0 +1,18 @@
+# For sentence polarity 
+
+
+#very negative
+sen = "Whenever someone is upset, they just need to toughen up and stop complaining. Emotions are just a sign of weakness. I don't see why people make such a big deal out of their feelings. It's all in their heads, and they should get over it."
+result = evaluate_emotional_intelligence(sen)
+print(result)
+#moderate
+sen = "I think emotions are important, but sometimes people can be overly sensitive. It's essential to consider other people's feelings, but we shouldn't dwell on our own emotions too much. Life has its ups and downs, and we should learn to cope with them."
+result = evaluate_emotional_intelligence(sen)
+print(result)
+#very positive 
+sen = "I believe that emotions are a fundamental part of being human. They provide us with valuable insights into our thoughts and needs. It's crucial to be empathetic and supportive when others are going through difficult times. At the same time, it's essential to recognize our emotions and find healthy ways to manage and express them. Emotional intelligence enables us to connect with others on a deeper level and navigate life's challenges with resilience."
+result = evaluate_emotional_intelligence(sen)
+print(result)
+
+
+
