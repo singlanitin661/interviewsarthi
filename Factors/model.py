0fdb6af9e431ec17ab3ecf6bd3b8e4ef075fa9e3
Nitin Singla <78803509+singlanitin661@users.noreply.github.com>
2023-10-10 15:42:18 +0530

    Create model.py

---
+++ Factors/model.py
@@ -0,0 +1,9 @@
+#import statements
+
+
+#factor functions: also Write what those functions are doing, just a breif summary 
+
+
+
+# main model :-> from where we are going to invoke all these factor functions
+#don't write anything here yet
