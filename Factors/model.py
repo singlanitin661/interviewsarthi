da7c7b5252c4f338aaeee5f59a637270298b40e1
Nitin Singla <78803509+singlanitin661@users.noreply.github.com>
2023-10-10 15:46:41 +0530

    Update model.py

--- Factors/model.py
+++ Factors/model.py
@@ -1,8 +1,25 @@
 #import statements
-
+pip install nltk
 
 #factor functions: also Write what those functions are doing, just a breif summary 
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('vader_lexicon')
+sia = SentimentIntensityAnalyzer()
 
+def evaluate_emotional_intelligence(answer):
+    # Tokenize the answer
+    tokens = word_tokenize(answer)
+
+    # Calculate sentiment scores
+    sentiment_scores = sia.polarity_scores(answer)
+
+    # Example of emotional intelligence assessment based on sentiment
+    # You can define your own criteria and weights
+    sentiment_score = sentiment_scores['compound']
+    return sentiment_score
 
 
 # main model :-> from where we are going to invoke all these factor functions
