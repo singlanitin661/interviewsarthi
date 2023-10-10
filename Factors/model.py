c4ab9ab0f87c4f35bc8781ca26587b85ebd6c3ec
Karandhar007 <78785551+Karandhar007@users.noreply.github.com>
2023-10-10 16:25:12 +0530

    Update model.py
    added BERT comparison and Grammatical error count

--- Factors/model.py
+++ Factors/model.py
@@ -1,5 +1,8 @@
-#import statements
+#install statements
 pip install nltk
+pip install sentence_transformers
+pip install -U git+https://github.com/KornWtp/ConGen.git
+pip install language-tool-python
 
 #factor functions: also Write what those functions are doing, just a breif summary 
 import nltk
@@ -8,6 +11,8 @@ from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 nltk.download('vader_lexicon')
 sia = SentimentIntensityAnalyzer()
+from sentence_transformers import SentenceTransformer , util             
+import language_tool_python
 
 
 #Used to evaluate the emotional intelligence or sentimental score of user's answer
@@ -23,6 +28,27 @@ def evaluate_emotional_intelligence(answer):
     sentiment_score = sentiment_scores['compound']
     return sentiment_score
 
+def compare_answers(user_ans,def_ans) :
+    # defining model to be used for comparison
+    model = SentenceTransformer("kornwtp/ConGen-BERT-Small")
+  
+    # Compute embedding for both
+    embedding_1= model.encode(user_ans, convert_to_tensor=True)
+    embedding_2 = model.encode(def_ans, convert_to_tensor=True)
+  
+    # Compare and genrate score 
+    tensor = util.pytorch_cos_sim(embedding_1, embedding_2)
+    return float(tensor)
+
+def num_gramm_errors(user_ans) :
+    # Create a LanguageTool instance
+    tool = language_tool_python.LanguageTool('en-US')
+  
+    # Check the grammar of the sentence
+    matches = tool.check(user_ans)
+  
+    # Return the number of grammar issues
+    return len(matches)
 
 # main model :-> from where we are going to invoke all these factor functions
 #don't write anything here yet
