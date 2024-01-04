a79bc60d51083f939e1da3ff04386781a3da413d
AryanJindal <jindalaryan070707@gmail.com>
2024-01-04 19:13:59 +0530

    A little of chatBox Fixation for empty columns

--- app/src/components/ChatBox.js
+++ app/src/components/ChatBox.js
@@ -6,15 +6,20 @@ const ChatBox = ({ role, message }) => {
   const convertToJSON = async (message) => {
     return JSON.parse(message);
   };
-  const [finalMessage, setFinalMessage] = useState("Error in parsing json");
+  const [continuationMessage, setcontinuationMessage] = useState("Error in parsing json");
   const [improvMessage, setImprovMessage] = useState("Error in parsing json");
+  const [questionMessage, setQuestionMessage] = useState("Error in parsing json");
   if (role !== "user" && message !== "understood") {
     convertToJSON(message)
       .then((res) => {
         console.log(res);
-        setFinalMessage(res["Continuations"]);
+        setcontinuationMessage(res["Continuations"]);
         setImprovMessage(res["Improvements"]);
-        console.log(finalMessage);
+        setQuestionMessage(res["Question"])
+        console.log(continuationMessage);
+        console.log(improvMessage);
+        console.log(questionMessage);
+
       })
       .catch((error) => {
         console.error("Error parsing JSON:", error);
@@ -23,16 +28,34 @@ const ChatBox = ({ role, message }) => {
 
   return (
     <div className="inline-block w-auto h-auto">
-      {improvMessage !== "Error in parsing json" && improvMessage && <p className="shadow-md my-2 p-4 rounded-lg bg-green-200 mr-[30vw] ml-[10vw]">{improvMessage}</p>}
-      <p className={ role === "user" ? "shadow-md my-2 p-4 bg-[#42bdfc] ml-[30vw] mr-[10vw] rounded-lg"  : "shadow-md my-2 p-4 rounded-lg bg-gray-200 mr-[30vw] ml-[10vw]"} >
-        {role === "user" || (message === "understood" && role !== "user")
-                 ? message
-                 : finalMessage
-  }
-      </p>
-      
+      {improvMessage !== "Error in parsing json" && improvMessage && (
+        <p className="shadow-md my-2 p-4 rounded-lg bg-green-200 mr-[30vw] ml-[10vw]">
+          {improvMessage}
+        </p>
+      )}
+  
+      {(role === "user" || (continuationMessage && continuationMessage !== "Error in parsing json")) && (
+        <p
+          className={
+            role === "user"
+              ? "shadow-md my-2 p-4 bg-[#42bdfc] ml-[30vw] mr-[10vw] rounded-lg"
+              : "shadow-md my-2 p-4 rounded-lg bg-gray-200 mr-[30vw] ml-[10vw]"
+          }
+        >
+          {role === "user" || (message === "understood" && role !== "user")
+            ? message
+            : continuationMessage}
+        </p>
+      )}
+  
+      {(questionMessage !== "Error in parsing json" && questionMessage ) && (
+        <p className="shadow-md my-2 p-4 rounded-lg bg-red-200 mr-[30vw] ml-[10vw]">
+          {questionMessage}
+        </p>
+      )}
     </div>
   );
+  
 };
 
 export default ChatBox;
