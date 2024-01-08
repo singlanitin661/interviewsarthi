db2cf47bcadd7fec24882983960b50fec82329a0
AryanJindal <jindalaryan070707@gmail.com>
2024-01-08 14:20:06 +0530

    Shimmer settlement

--- app/src/components/Shimmer.js
+++ app/src/components/Shimmer.js
@@ -24,7 +24,7 @@ const Shimmer = () => {
 
   return (
     <div
-      className={`inline-block bg-[#5aa9e6]  max-w-[40vw] ml-[10vw] mb-5 min-h-[23vh] max-h-[23vh] rounded-lg 'animate-shimmer'`}
+      className={`inline-block bg-[#5aa9e6]  max-w-[40vw] ml-[10vw] mb-5 min-h-[23vh] max-h-[23vh] rounded-lg animate-shimmer flex flex-col justify-center align-center`}
       style={{ position: 'static', overflow: 'hidden' }}
     >
       <style>{shimmerStyles}</style>
